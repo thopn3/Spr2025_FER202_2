887466d6c62f6ee068e9224a593a74ae7abffbd3
thopn3 <thopn3@fpt.edu.vn>
2025-03-05 13:44:33 +0700

    Add to cart

--- work_json_server/src/components/Product.js
+++ work_json_server/src/components/Product.js
@@ -5,11 +5,48 @@ import { Link } from "react-router-dom";
 
 function Product() {
     const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState(null);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:9999/products")
             .then(result => setProducts(result.data))
     }, []);
+
+    const handleAddCart = (id) => {
+        axios.get("http://localhost:9999/products/"+id)
+            .then(res => setProduct(res.data))
+            .catch(err=>console.error(err));
+        
+        // Kiểm tra product != null
+        if(product!=null){
+            // Kiểm tra nếu trong cart đã có sản phẩm trước đó thì tăng quantity. Ngược lại, thêm vào cart
+            if(localStorage.getItem('cart')!=null){
+                setCart(JSON.parse(localStorage.getItem('cart')));
+
+                // Duyệt từng item trong cart, so sánh id = id (tham số) không
+                cart?.map(item => {
+                    if(item.id == id){
+                        item.quantity++; // Tăng số lượng nếu đã tồn tại
+                    }else{
+                        // Thêm item mới vào cart
+                        const newItem = {id:product.id,name:product.name,price:product.price,quantity:1};
+                        setCart([...cart, newItem]);
+                    }
+                });
+                // Cập nhật lại localStorage
+                localStorage.setItem('cart', JSON.stringify(cart));
+                
+            }else{
+                // Trường hợp chưa có key 'cart' trong localStorage
+                localStorage.setItem('cart', JSON.stringify(
+                    [
+                        {id:product.id,name:product.name,price:product.price,quantity:1}
+                    ]
+                ));
+            }
+        }
+    }
 
     return (
         <Container fluid>
@@ -21,6 +58,12 @@ function Product() {
             <Row>
                 <Col>
                     <h1>Product List</h1>
+                    <div style={{textAlign:"right"}}>
+                        Cart: 
+                        <Link to={'/cart'}>
+                            {cart.length > 0 ? cart.length : <span>0</span>}
+                        </Link>
+                    </div>
                     <Table hover bordered striped>
                         <thead>
                             <tr>
@@ -45,12 +88,12 @@ function Product() {
                                         <td>
                                             <ul>
                                                 {
-                                                    p?.category?.map(c => (<li>{c}</li>))
+                                                    p?.category?.map(c => (<li key={c}>{c}</li>))
                                                 }
                                             </ul>
                                         </td>
                                         <td>
-                                            <Button>Buy Now</Button>
+                                            <Button onClick={()=>handleAddCart(p?.id)}>Buy Now</Button>
                                         </td>
                                     </tr>
                                 ))
